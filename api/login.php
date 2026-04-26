45179196942354d96dfda688c1c45b697af03ab8
aisyahnarl <aisyahnilmi@gmail.com>
2026-04-27 01:14:27 +0700

    fixing login

--- api/login.php
+++ api/login.php
@@ -2,11 +2,11 @@
 session_start();
 include 'config.php'; 
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-var_dump(session_status()); // harusnya int(2) = PHP_SESSION_ACTIVE
-var_dump(session_id());      // harusnya ada string panjang
-die();
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// var_dump(session_status()); // harusnya int(2) = PHP_SESSION_ACTIVE
+// var_dump(session_id());      // harusnya ada string panjang
+// die();
 ?>
 <!DOCTYPE html>
 <html lang="id">
